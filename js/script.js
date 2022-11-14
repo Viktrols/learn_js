b1aa6d73d6082ccdcf4257af24f47a663f0da1a6
Viktoria Lupanova <viktoriia.lupanova@gmail.com>
2022-11-14 22:09:55 +0100

    object and prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+let numberOfFilms = +prompt('Cколько фильмов ты уже посмотрел?', '');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    gemres: [],
+    private: false
+};
 
+let lastMovie = prompt('Один из последних фильмов?','');
+let lastMovieRate = +prompt('Оцените его?', '');
+
+personalMovieDB.movies[lastMovie] = lastMovieRate;
+console.log(personalMovieDB);
