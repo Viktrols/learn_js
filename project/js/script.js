12807b9e3c4500c29e356a5a6df82cd525810b20
Viktoria Lupanova <viktoriia.lupanova@gmail.com>
2022-12-24 00:30:30 +0100

    Marsianin task.

--- project/js/script.js
+++ project/js/script.js
@@ -19,8 +19,37 @@ const movieDB = {
         "Логан",
         "Лига справедливости",
         "Ла-ла лэнд",
+        "Шрек",
         "Одержимость",
         "Скотт Пилигрим против..."
     ]
 };
 
+// const advertising = document.querySelector('.promo__adv');
+// advertising.remove();
+
+const allAdvertising = document.querySelectorAll('.promo__adv img');
+allAdvertising.forEach(elem => elem.remove());
+
+
+const promoGenre = document.querySelector('.promo__genre');
+// promoGenre.innerHTML = 'драма';
+promoGenre.textContent = 'драма';
+
+const promoBg = document.querySelector('.promo__bg');
+promoBg.style.background = "url('img/bg.jpg')";
+promoBg.style.backgroundSize = "100%";
+
+const promoInterList = document.querySelector('.promo__interactive-list');
+promoInterList.innerHTML = '';
+movieDB.movies.sort();
+
+movieDB.movies.forEach(function(item, index) {
+    // const li = document.createElement('li');
+    // li.classList.add('promo__interactive-item');
+    // li.innerHTML = `${index+1}. ${item}<div class="delete"></div>`;
+    // promoInterList.append(li);
+    promoInterList.innerHTML += `<li class="promo__interactive-item">${index+1}. ${item}
+                 <div class="delete"></div>
+                 </li>`;
+});
